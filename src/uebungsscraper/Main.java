2ed7fcfb3fa67ff5c782e0062efbdb164839bae1
Dominik <odrljind@vis.ethz.ch>
2018-10-13 22:17:43 +0200

    added better exception handling

--- src/uebungsscraper/Main.java
+++ src/uebungsscraper/Main.java
@@ -1,23 +1,25 @@
 package uebungsscraper;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.io.FileUtils;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		int linkCounter = 0;
-		int downloadCounter = 0;
-
-		String userAgent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+		AtomicInteger linkCounter = new AtomicInteger();
+		AtomicInteger downloadCounter = new AtomicInteger();
 
 		// START AnD
 		// Exercises
@@ -30,12 +32,16 @@ public class Main {
 		for (Element tag : adRelevantTags) {
 			String sTag = tag.toString();
 			if (sTag.contains("Programmieraufgaben") || sTag.contains("Vorlage") || sTag.contains("Anleitung")) {
-				downloadCounter += downloadFromTag(tag, "AD/Programmieraufgaben/", adBuilderUrl) ? 1 : 0;
-				linkCounter += 1;
+				if (downloadFromTag(tag, "AD/Programmieraufgaben/", adBuilderUrl)) {
+					downloadCounter.incrementAndGet();
+				}
+				linkCounter.incrementAndGet();
 			} else {
 				if (!sTag.contains("skript.pdf")) {
-					downloadCounter += downloadFromTag(tag, "AD/Uebungen/", adBuilderUrl) ? 1 : 0;
-					linkCounter += 1;
+					if (downloadFromTag(tag, "AD/Uebungen/", adBuilderUrl)) {
+						downloadCounter.incrementAndGet();
+					}
+					linkCounter.incrementAndGet();
 				}
 			}
 		}
@@ -46,8 +52,10 @@ public class Main {
 			String sTag = tag.toString();
 			if (!(sTag.contains("index.html") || sTag.contains("moodle") || sTag.contains("disclaimer.html")
 					|| sTag.contains("copyright.html") || sTag.contains("skript.pdf"))) {
-				downloadCounter += downloadFromTag(tag, "AD/Slides/", adBuilderUrl) ? 1 : 0;
-				linkCounter += 1;
+				if (downloadFromTag(tag, "AD/Slides/", adBuilderUrl)) {
+					downloadCounter.incrementAndGet();
+				}
+				linkCounter.incrementAndGet();
 			}
 		}
 		// END AnD
@@ -57,24 +65,43 @@ public class Main {
 		String dmRelevantHtml = readStringFromURL(dmMasterUrl).split("tter</h3>", 2)[1].split("bungsgruppen</h3", 2)[0];
 		ArrayList<Element> dmRelevantTags = Jsoup.parse(dmRelevantHtml, "UTF-8").select("a[href]");
 		for (Element tag : dmRelevantTags) {
-			downloadFromTag(tag, "DiskMat/Uebungen/", dmMasterUrl);
+			if (downloadFromTag(tag, "DiskMat/Uebungen/", dmMasterUrl)) {
+				downloadCounter.incrementAndGet();
+			}
+			linkCounter.incrementAndGet();
 		}
 		// END DiskMath
 
 		// START EProg
 		String epMasterUrl = "http://www.lst.inf.ethz.ch/education/einfuehrung-in-die-programmierung-i--252-0027-.html";
-		String epRelevantHtml = readStringFromURL(epMasterUrl).split("e contains-textimage\">", 2)[1]
-				.split("<a class=\"accordionAnchor\" href=\"#\"><span class=\"title\">Lit", 2)[0];
+		String epRelevantHtml = "";
+		int countTries = 0;
+		while (true) {
+			try {
+				epRelevantHtml = readStringFromURL(epMasterUrl).split("e contains-textimage\">", 2)[1]
+						.split("<a class=\"accordionAnchor\" href=\"#\"><span class=\"title\">Lit", 2)[0];
+				break;
+			} catch (FileNotFoundException e) {
+				if (++countTries == 3) {
+					System.out.println("\n===== [EProg failed badly - usually you can fix this by trying again] =====\n"
+							+ e + "\n");
+					break;
+				}
+			}
+		}
 		ArrayList<Element> epRelevantTags = Jsoup.parse(epRelevantHtml, "UTF-8").select("a[href]");
 		for (Element tag : epRelevantTags) {
 			if (tag.toString().contains("18E")) {
-				downloadCounter += downloadFromTag(tag, "EProg/Slides/", "") ? 1 : 0;
-				;
-				linkCounter += 1;
+				if (downloadFromTag(tag, "EProg/Slides/", "")) {
+					downloadCounter.incrementAndGet();
+				}
+				linkCounter.incrementAndGet();
 			} else if (tag.attr("href").toString().contains("#")) {
 			} else {
-				downloadCounter += downloadFromTag(tag, "EProg/Uebungen/", "") ? 1 : 0;
-				linkCounter += 1;
+				if (downloadFromTag(tag, "EProg/Uebungen/", "")) {
+					downloadCounter.incrementAndGet();
+				}
+				linkCounter.incrementAndGet();
 			}
 		}
 		// END EProg
@@ -83,24 +110,33 @@ public class Main {
 		// Exercises
 		String laMasterUrl = "http://igl.ethz.ch/teaching/linear-algebra/la2018/";
 		String laHtml = readStringFromURL(laMasterUrl);
-		String laRelevantHtml = laHtml.split("bungen</h3>", 2)[1].split("<h3>Übungsgruppen</h3>", 2)[0];
+		String laRelevantHtml = laHtml.split("bungen</h3>", 2)[1].split("bungsgruppen</h3>", 2)[0];
 		ArrayList<Element> laRelevantTags = Jsoup.parse(laRelevantHtml, "UTF-8").select("a[href]");
 		for (Element tag : laRelevantTags) {
-			downloadFromTag(tag, "LinAlg/Uebungen/", laMasterUrl);
+			if (downloadFromTag(tag, "LinAlg/Uebungen/", laMasterUrl)) {
+				downloadCounter.incrementAndGet();
+			}
+			linkCounter.incrementAndGet();
 		}
-		// Notes
 		String lanRelevantHtml = laHtml.split("bersicht der Vorlesung</h3>", 2)[1]
 				.split("<p><a class=\"a-int\" href=\"https://www.vid", 2)[0];
 		ArrayList<Element> lanRelevantTags = Jsoup.parse(lanRelevantHtml, "UTF-8").select("a[href]");
 		for (Element tag : lanRelevantTags) {
-			downloadFromTag(tag, "LinAlg/Notes/", laMasterUrl);
+			if (downloadFromTag(tag, "LinAlg/Notes/", laMasterUrl)) {
+				downloadCounter.incrementAndGet();
+			}
+			linkCounter.incrementAndGet();
 		}
 		// END LinAlg
 
+		// Print results - end of program
+		String[] thankyous = { "Thank you for flying Emirates!",
+				"Thank you for flying Delta Business Express. We hope you enjoyed giving us the business as much as we enjoyed taking you for a ride.",
+				"Thank you for smoking!", "tank you come again",
+				"I know where you live " + System.getProperty("user.name"), "All your exercises are belong to us!" };
 		System.out.println();
-		System.out.println(linkCounter + " links discovered and " + downloadCounter + " downloaded.");
-		System.out.println("Thank you for flying Emirates!");
-
+		System.out.println(linkCounter.get() + " links discovered and " + downloadCounter.get() + " downloaded.");
+		System.out.println(thankyous[new Random().nextInt(thankyous.length)]);
 	}
 
 	public static boolean downloadFromTag(Element tag, String directory, String builderUrl) throws IOException {
@@ -110,9 +146,15 @@ public class Main {
 		URL url = new URL((builderUrl + link).replace(" ", "%20"));
 		File file = new File(filename);
 		if (!new File(filename).isFile()) {
-			FileUtils.copyURLToFile(url, file);
-			System.out.println("[Downloaded " + directory.split("/")[0] + "] " + url + " into " + filename);
-			return true;
+			try {
+				FileUtils.copyURLToFile(url, file);
+				System.out.println("[Downloaded " + directory.split("/")[0] + "] " + url + " into " + filename);
+				return true;
+			} catch (HttpStatusException | FileNotFoundException e) {
+				System.out.println("\n[Failed " + directory.split("/")[0] + "] " + url);
+				System.out.println("[Excption] " + e + "\n");
+				return false;
+			}
 		} else {
 			System.out.println("[Skipped " + directory.split("/")[0] + "] " + url);
 			return false;
@@ -125,5 +167,4 @@ public class Main {
 			return scanner.hasNext() ? scanner.next() : "";
 		}
 	}
-
 }
